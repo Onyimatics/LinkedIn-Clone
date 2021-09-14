afa73f088eebd0c584265c12e127757846d8ce9c
Onyimatics <ezikeonyinyefavour@gmail.com>
2021-09-14 21:59:13 +0200

    nestjs e2e test with supertest

--- api/src/auth/services/auth.service.ts
+++ api/src/auth/services/auth.service.ts
@@ -1,11 +1,13 @@
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import {from, map, Observable, of, switchMap, tap} from 'rxjs';
-import { User } from '../models/user.class';
-import { UserEntity } from '../models/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { from, Observable, of } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.class';
 
 @Injectable()
 export class AuthService {
@@ -19,82 +21,81 @@ export class AuthService {
     return from(bcrypt.hash(password, 12));
   }
 
-    doesUserExist(email: string): Observable<boolean> {
-        return from(this.userRepository.findOne({ email })).pipe(
-            switchMap((user: User) => {
-                return of(!!user);
-            }),
-        );
-    }
+  doesUserExist(email: string): Observable<boolean> {
+    return from(this.userRepository.findOne({ email })).pipe(
+      switchMap((user: User) => {
+        return of(!!user);
+      }),
+    );
+  }
 
-    registerAccount(user: User): Observable<User> {
-        const { firstName, lastName, email, password } = user;
+  registerAccount(user: User): Observable<User> {
+    const { firstName, lastName, email, password } = user;
 
-        return this.doesUserExist(email).pipe(
-            tap((doesUserExist: boolean) => {
-                if (doesUserExist)
-                    throw new HttpException(
-                        'A user has already been created with this email address',
-                        HttpStatus.BAD_REQUEST,
-                    );
-            }),
-            switchMap(() => {
-                return this.hashPassword(password).pipe(
-                    switchMap((hashedPassword: string) => {
-                        return from(
-                            this.userRepository.save({
-                                firstName,
-                                lastName,
-                                email,
-                                password: hashedPassword,
-                            }),
-                        ).pipe(
-                            map((user: User) => {
-                                delete user.password;
-                                return user;
-                            }),
-                        );
-                    }),
-                );
-            }),
+    return this.doesUserExist(email).pipe(
+      tap((doesUserExist: boolean) => {
+        if (doesUserExist)
+          throw new HttpException(
+            'A user has already been created with this email address',
+            HttpStatus.BAD_REQUEST,
+          );
+      }),
+      switchMap(() => {
+        return this.hashPassword(password).pipe(
+          switchMap((hashedPassword: string) => {
+            return from(
+              this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+              }),
+            ).pipe(
+              map((user: User) => {
+                delete user.password;
+                return user;
+              }),
+            );
+          }),
         );
-    }
+      }),
+    );
+  }
 
-    validateUser(email: string, password: string): Observable<User> {
-        return from(
-            this.userRepository.findOne(
-                { email },
-                {
-                    select: ['id', 'firstName', 'lastName', 'email', 'password', 'role'],
-                },
-            ),
-        ).pipe(
-            switchMap((user: User) => {
-                if (!user) {
-                    // throw new HttpException('Not found', HttpStatus.NOT_FOUND);
-                    throw new HttpException(
-                        { status: HttpStatus.NOT_FOUND, error: 'Invalid Credentials' },
-                        HttpStatus.NOT_FOUND,
-                    );
-                }
-                return from(bcrypt.compare(password, user.password)).pipe(
-                    map((isValidPassword: boolean) => {
-                        if (isValidPassword) {
-                            delete user.password;
-                            return user;
-                        }
-                    }),
-                );
-            }),
+  validateUser(email: string, password: string): Observable<User> {
+    return from(
+      this.userRepository.findOne(
+        { email },
+        {
+          select: ['id', 'firstName', 'lastName', 'email', 'password', 'role'],
+        },
+      ),
+    ).pipe(
+      switchMap((user: User) => {
+        if (!user) {
+          throw new HttpException(
+            { status: HttpStatus.FORBIDDEN, error: 'Invalid Credentials' },
+            HttpStatus.FORBIDDEN,
+          );
+        }
+        return from(bcrypt.compare(password, user.password)).pipe(
+          map((isValidPassword: boolean) => {
+            if (isValidPassword) {
+              delete user.password;
+              return user;
+            }
+          }),
         );
-    }
+      }),
+    );
+  }
 
   login(user: User): Observable<string> {
     const { email, password } = user;
     return this.validateUser(email, password).pipe(
       switchMap((user: User) => {
         if (user) {
-          // create JWT credentials
+          // create JWT - credentials
           return from(this.jwtService.signAsync({ user }));
         }
       }),
